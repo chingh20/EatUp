83dd606ec70c0de597794554ec854b65cff7eb36
janjanchen <ssp.18s20.janicechen@gmail.com>
2021-05-25 03:01:26 +0800

    test1

--- eatUp/src/Functional/Login System/Login.js
+++ eatUp/src/Functional/Login System/Login.js
@@ -1,6 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
   Image,
   TextInput,
@@ -11,52 +9,41 @@ import {
   View
 } from 'react-native';
 
+const Login = () => {
 
-export default function Login() {
-   const Login = (props) => {
-     const [email, setEmail] = useState('')
-     const [password, setPassword] = useState('')
+//  const [email, setEmail] = useState('')
+//  const [password, setPassword] = useState('')
+//
+//  const handleEmailUpdate = (text) => setEmail(text)
+//  const handlePasswordUpdate = (text) => setPassword(text)
+//
+//  const handleButtonPress = () => {
+//    setEmail('')
+//    setPassword('')
+//     }
 
-     const handleEmailUpdate = (text) => setEmail(text)
-     const handlePasswordUpdate = (text) => setPassword(text)
-
-     const handleButtonPress = () => {
-       setEmail('')
-       setPassword('')
-     }
-
-     return(
-       <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? "padding" : "height"} style={styles.container}>
-         <Image style={styles.image} source={require('../../assets/logo.png')} />
-         <TextInput
-           style={styles.textInput}
-           placeholder="Name"
-           onChangeText={handleNameUpdate}
-           value={name}
-         />
-         <TextInput
-           style={styles.textInput}
-           placeholder="Email"
-           onChangeText={handleEmailUpdate}
-           value={email}
-           keyboardType="email-address"
-         />
-         <TextInput
-           style={styles.textInput}
-           placeholder="Password"
-           onChangeText={handlePasswordUpdate}
-           value={password}
-           secureTextEntry
-         />
-
-  return (
-
-    <View style={styles.container}>
-      <Image style={styles.image} source={require("./assets/splash.png")} />
-      <Text>Login Page 123</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+     return (
+//       <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? "padding" : "height"} style={styles.container}>
+//         <TextInput
+//           style={styles.textInput}
+//           placeholder="Email"
+//           onChangeText={handleEmailUpdate}
+//           value={email}
+//           keyboardType="email-address"
+//         />
+//         <TextInput
+//           style={styles.textInput}
+//           placeholder="Password"
+//           onChangeText={handlePasswordUpdate}
+//           value={password}
+//           secureTextEntry
+//         />
+//       </KeyboardAvoidingView>
+//     );
+ <View style={styles.container}>
+        <Text style={styles.textInput}>hello</Text>
+ </View>
+ );
 }
 
 
