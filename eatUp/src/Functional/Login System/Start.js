a4067b626d81fd2e3bf29921e945f74bcbff9de5
janjanchen <ssp.18s20.janicechen@gmail.com>
2021-05-25 17:06:33 +0800

    test1

--- eatUp/src/Functional/Login System/Start.js
+++ eatUp/src/Functional/Login System/Start.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
-import Login from './Login';
+//import Login from './Login';
+//import Signup from './Signup';
 
 
 
 const Start = ({navigation}) => {
   return (
      <View style={styles.container}>
-     <Image style={styles.image} source = {require("./Logo/logo.png")}/>
+     <Image style={styles.image} source = {require("../../../assets/logo-full-background.png")}/>
      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Login')}>
-     <Text style={styles.btnText}>Log In</Text>
+        <Text style={styles.btnText}>Log In</Text>
      </TouchableOpacity>
+
+     <Text style ={styles.nobutton}> No account yet? Sign up
+     <TouchableOpacity style={styles.nobutton} onPress={() => navigation.navigate('Signup')}>
+             <Text style={styles.nobuttontext}> here!</Text>
+          </TouchableOpacity>
+     </Text>
      </View>
   );
 };
@@ -30,6 +37,18 @@ const styles = StyleSheet.create({
      padding: 15,
      borderRadius: 50,
   },
+  nobutton: {
+       color: '#3e1f0d',
+       fontSize: 20,
+       marginTop: 30,
+       alignItems: 'center',
+       justifyContent: 'center',
+    },
+
+  nobuttontext: {
+       color: '#bc1824',
+       fontSize: 20,
+    },
   btnText: {
       color: "white",
       fontSize: 20,
@@ -37,13 +56,14 @@ const styles = StyleSheet.create({
       textAlign: "center",
    },
    image: {
-        height: 250,
-        width: 350,
+      height: 250,
+      width: 350,
+      marginBottom: 10
    },
    textInput: {
-         color: "black",
+         color: '#3e1f0d',
          borderWidth: 1,
-         borderColor: 'black',
+         borderColor: '#3e1f0d',
          fontSize: 20,
          marginBottom: 8,
          width: 200,
